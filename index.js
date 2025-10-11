fd01067bab0493cc822ee1e4c84cda059e7072ae
sparkmahadi <mahadi.developer@gmail.comm>
2025-10-12 00:49:19 +0600

    fixed cors issue

--- index.js
+++ index.js
@@ -11,24 +11,7 @@ const { connectToDB } = require('./db');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  'https://modern-poultry-client.vercel.app',
-  'http://localhost:3000',
-];
-
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
-
+app.use(cors({ origin: '*' }));;
 app.use(express.json({ limit: '10mb' }));
 
 // Connect to MongoDB
