2b835597d798f796fdfc580cc06a59d4549ab819
sparkmahadi <mahadi.developer@gmail.comm>
2025-10-15 22:41:58 +0600

    changed cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { connectToDB } = require('./db');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: '*' }));;
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 
 // Connect to MongoDB
